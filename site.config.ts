7757c5cc18ade0458b895b5deaef87ef8e9150cf
s.pierre <s.pierre@criteo.com>
2023-12-14 11:06:50 +0100

    config v2

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'template test',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Guide de Pierre le bg',
+  domain: 'pierre-le-bg.it',
+  author: 'SAMSON Pierre',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Guide de Pierre le bg pour faire les projets',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'SAMSONPierre',
+  linkedin: 'pierre-samson',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
